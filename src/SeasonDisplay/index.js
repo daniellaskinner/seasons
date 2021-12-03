2fd335bf12b682eaf9e2c1bae2574dd0619a5a68
nelly.skinner <daniellaskinner120@gmail.com>
2021-12-03 17:51:08 +0000

    Add lifecycle method to index, refactor season display to class componet

--- src/SeasonDisplay/index.js
+++ src/SeasonDisplay/index.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
-const SeasonDisplay = () => {
-  return <div>poop</div>
+class SeasonDisplay extends React.Component {
+
+  //TODO: set props here to pass from parent component- lat and long
+  // use these props to update state
+  // OR: code to determine season live in index and pass down as props to seasonDisplay.
+
+  state = {
+    lat: false,
+    long: false,
+    winter: true
+  }
+
+  componentDidMount() {
+    // if current month is sept- march and lat fits with criteria
+    if (this.state.lat > 30 && this.state.long > 50) {
+      this.setState({winter: !this.state.winter})
+    }
+  }
+
+  render() {
+    return <div>{this.state.winter ? 'It\'s chilly, wrap up!' : 'Its hot hot hot!'}</div>
+  }
 }
 
 export default SeasonDisplay
--- src/index.js
+++ src/index.js
@@ -2,25 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 
-// code to determine location and month to output 'winter' or 'summer'
-// pass w or s through props to display
 class App extends React.Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      lat: null,
-      long: null
-    }
+  state = {
+    lat: false,
+    errorMessage: false,
+    isLoading: false
   }
 
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    )
+  componentDidMount() {
+    if (!this.state.errorMessage && !this.state.lat) {
+      this.setState({isLoading: true})
+    }
 
-    return <div>Latitude: </div>
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({lat: position.coords.latitude, long: position.coords.longitude, isLoading: false}),
+      (err) => this.setState({errorMessage: err.message})
+    )
+  }
+
+  // TODO: only want to display line 27 OR line 28, alter this conditional.
+  render() {
+    return <div>
+      {this.state.errorMessage ? ('Error: ' + this.state.errorMessage) : <SeasonDisplay/>}
+      {this.state.isLoading ? 'Loading' : null}
+    </div>
   }
 }
 
